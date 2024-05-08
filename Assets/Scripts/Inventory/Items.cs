8bfd6ade7d855ed6f12d610ea7e6e49a8ca9ed0e
Tushev-Dmitriy <110030309+Tushev-Dmitriy@users.noreply.github.com>
2024-05-08 09:55:14 +0300

    update SO system for inventory

--- Assets/Scripts/Inventory/Items.cs
+++ Assets/Scripts/Inventory/Items.cs
@@ -1,58 +1,59 @@
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-[CreateAssetMenu(fileName = "Resource", menuName = "Inventory/Resource")]
-[System.Serializable]
-public class Resource: ScriptableObject
-{
-    public Sprite icon;
-    public string title;
-    public int cost;
-    public int requiredLevel;
-    public int timeToSpawn;
-    public int quantity;
-    public int xp;
-}
-
-
-
-[CreateAssetMenu(fileName = "Mineral", menuName = "Inventory/Mineral")]
-[System.Serializable]
-public class Mineral: ScriptableObject
-{
-    public string title;
-    public int xp;
-    public int quantity;
-    public int timeToSpawn;
-}
+//[CreateAssetMenu(fileName = "Resource", menuName = "Inventory/Resource")]
+//[System.Serializable]
+//public class Resource : ScriptableObject
+//{
+//    public Sprite icon;
+//    public string title;
+//    public int cost;
+//    public int requiredLevel;
+//    public int timeToSpawn;
+//    public int quantity;
+//    public int xp;
+//}
 
 
 
-[CreateAssetMenu(fileName = "Tool", menuName = "Inventory/Tool")]
-[System.Serializable]
-public class Tool: ScriptableObject
-{
-    public string title;
-    public int cost;
-    public int durability;
-    public int requiredLevel;
-}
+//[CreateAssetMenu(fileName = "Mineral", menuName = "Inventory/Mineral")]
+//[System.Serializable]
+//public class Mineral : ScriptableObject
+//{
+//    public string title;
+//    public int xp;
+//    public int quantity;
+//    public int timeToSpawn;
+//}
 
 
 
-[System.Serializable]
-[CreateAssetMenu(fileName = "CraftItem", menuName = "Inventory/CraftItem")]
-public class CraftableItem: ScriptableObject
-{
-    public string title;
-    public List<Recipe> recipe;
-    public int cost;
-    public int requiredLevel;
-}
+//[CreateAssetMenu(fileName = "Tool", menuName = "Inventory/Tool")]
+//[System.Serializable]
+//public class Tool : ScriptableObject
+//{
+//    public string title;
+//    public int cost;
+//    public int durability;
+//    public int requiredLevel;
+//}
 
-[System.Serializable]
-public class Recipe
-{
-    public string resource;
-    public int quantity;
-}
+
+
+//[System.Serializable]
+//[CreateAssetMenu(fileName = "CraftItem", menuName = "Inventory/CraftItem")]
+//public class CraftableItem : ScriptableObject
+//{
+//    public string title;
+//    public List<Recipe> recipe;
+//    public int cost;
+//    public int requiredLevel;
+//}
+
+//[System.Serializable]
+//public class Recipe
+//{
+//    public string resource;
+//    public int quantity;
+//}
---
+++ Assets/Scripts/Inventory/Resource.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Resource", menuName = "Inventory/Resource")]
+[System.Serializable]
+public class Resource: ScriptableObject
+{
+    public Sprite icon;
+    public string title;
+    public int cost;
+    public int requiredLevel;
+    public int timeToSpawn;
+    public int quantity;
+    public int xp;
+}
